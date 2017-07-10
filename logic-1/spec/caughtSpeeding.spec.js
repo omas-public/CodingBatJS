a2d4082c3eecafe7820aab9930f177ea43366b62
Omas Naohiko <omas@std.it-college.ac.jp>
2017-07-10 17:02:47 +0900

    fix caughtSpeeding

--- logic-1/spec/caughtSpeeding.spec.js
+++ logic-1/spec/caughtSpeeding.spec.js
@@ -1,31 +1,27 @@
 /*
-You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday 
+You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday
 */
 
-const on that day, your speed can be 5 higher in all cases = . => {
+const caughtSpeeding = (speed, isBirthday) => {
   // Write code here
 
 };
-    
-describe('on that day, your speed can be 5 higher in all cases.', () => {
 
-    it(')', () => {
-      expect().toBe(undefined);
-    });
-    
+describe('caughtSpeeding(speed, isBirthday)', () => {
+
 
     it('caughtSpeeding(60, false) -> 0)', () => {
       expect(caughtSpeeding(60, false)).toBe(0);
     });
-    
+
 
     it('caughtSpeeding(65, false) -> 1)', () => {
       expect(caughtSpeeding(65, false)).toBe(1);
     });
-    
+
 
     it('caughtSpeeding(65, true) -> 0)', () => {
       expect(caughtSpeeding(65, true)).toBe(0);
     });
-    
+
 });
