d1a29b516654c7e226b24df4c32dc4e8c76090b0
Omas Naohiko <omas@std.it-college.ac.jp>
2017-07-10 18:41:04 +0900

    fix warmup-1 or35

--- warmup-1/spec/or35.spec.js
+++ warmup-1/spec/or35.spec.js
@@ -1,31 +1,27 @@
 /*
-Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator 
+Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator
 */
 
-const see Introduction to Mo = d => {
+const or35 = (n) => {
   // Write code here
 
 };
-    
-describe('see Introduction to Mod', () => {
 
-    it(')', () => {
-      expect().toBe(undefined);
-    });
-    
+describe('or35(n)', () => {
+
 
     it('or35(3) -> true)', () => {
       expect(or35(3)).toBe(true);
     });
-    
+
 
     it('or35(10) -> true)', () => {
       expect(or35(10)).toBe(true);
     });
-    
+
 
     it('or35(8) -> false)', () => {
       expect(or35(8)).toBe(false);
     });
-    
+
 });
